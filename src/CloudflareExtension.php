1dd8df20d95af84ba5d820dcbdd91d63b8f46c73
Chris Hilsdon <chris@koolserve.uk>
2016-06-14 21:21:49 +0100

    Created new project with Günther

---
+++ src/CloudflareExtension.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Bolt\Extension\Koolserve\Cloudflare;
+
+use Bolt\Extension\SimpleExtension;
+
+/**
+ * Cloudflare extension class.
+ *
+ * @author Chris Hilsdon <chris@koolserve.uk
+ */
+class CloudflareExtension extends SimpleExtension
+{
+
+}
